886a5f9251e24790065315048ce2e3f15a58382a
zhangxiaodang <961590849@qq.com>
2019-12-24 10:30:42 +0800

    增加新人专区功能

--- public/manager/assets/pages/scripts/data.js
+++ public/manager/assets/pages/scripts/data.js
@@ -795,7 +795,7 @@ function getNewbornContent(data,callback){
         type:"post",
         contentType:"application/json",
         async:true,        //异步请求（同步请求将会锁住浏览器，用户其他操作必须等待请求完成才可以执行）
-        url:webUrl + "newborn/newborndetail",  //请求发送到TestServlet处
+        url:webUrl + "newborn/detail",  //请求发送到TestServlet处
         data:sendMessageEdit(DEFAULT, data),
         dataType:"json",      //返回数据形式为json
         success:function(result){
